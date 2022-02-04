66189b1ed1a175a6e2a928adbb1cfb759818fd44
Wolfram-180 <sergei.ulvis@gmail.com>
2022-02-04 16:34:37 +0300

    gradle version upgraded

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,11 +5,16 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (ctx, index) => Container(
-        padding: EdgeInsets.all(8),
-        child: Text('This works!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This works!'),
+        ),
       ),
     );
   }
