433b38850bad0871a5b2d8133ef43f6335dfd659
Wolfram-180 <sergei.ulvis@gmail.com>
2022-02-07 16:03:15 +0300

    AuthForm widget added

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,22 @@
+import 'package:chat_firebase/widgets/auth/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(),
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({Key? key}) : super(key: key);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              child: Text(''),
+            ),
+          ),
+        ),
+      ),
+    ),
+  }
+}
