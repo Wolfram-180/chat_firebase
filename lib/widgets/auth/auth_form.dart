d0a2a7fe8b5e33c3b861851d31f3e4beb715c30d
Wolfram-180 <sergei.ulvis@gmail.com>
2022-02-08 21:43:16 +0300

    login form updated

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,21 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState?.validate();
+    FocusScope.of(context).unfocus();
+    if (isValid == true) {
+      _formKey.currentState?.save();
+      //
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,31 +32,76 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty == true ||
+                          value.contains('@') == false) {
+                        return 'Please enter valid email address';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(labelText: 'Email address'),
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty == true ||
+                          value.length < 7) {
+                        return 'Please enter valid user name with at least 7 symbols';
+                      }
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: 'User name'),
+                    onSaved: (value) {
+                      _userName = value!;
+                    },
                   ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: 'Password'),
-                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty == true ||
+                            value.length < 4) {
+                          return 'Please enter valid password with at least 4 symbols';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                      onSaved: (value) {
+                        _userPassword = value!;
+                      },
+                    ),
                   SizedBox(
                     height: 12,
                   ),
                   RaisedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
+                    onPressed: _trySubmit,
+                    child: Text(
+                      _isLogin == true ? 'Login' : 'Sign up',
+                    ),
                   ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    onPressed: () {},
-                    child: Text('Create new account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(
+                      _isLogin == true
+                          ? 'Create new account'
+                          : 'I already have an account',
+                    ),
                   ),
                 ],
               ),
